dec13bc58dc21f6733b14c7d9f98703341a1e218
viniRsilva <vinicius14082004@gmail.com>
2024-11-20 20:02:59 -0300

    finalizacao crud departamento

--- src/data/departamento/DepartamentoEntity.java
+++ src/data/departamento/DepartamentoEntity.java
@@ -1,8 +1,18 @@
 package data.departamento;
 
+import domain.model.Departamento;
+
 public record DepartamentoEntity(
         String nome,
         double limite,
         int idDepartamento
 ){
+    public DepartamentoEntity(Departamento departamento) {
+        this(
+                departamento.nome(),
+                departamento.limite(),
+                departamento.id()
+        );
+    }
 }
+
