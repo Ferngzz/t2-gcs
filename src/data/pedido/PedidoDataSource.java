8958d362f88e593b9e8e74bdaa63cedd5b68a566
Luiz <kubaszewski001@hotmail.com>
2024-11-20 20:43:37 -0300

    Criando o CRUD de pedidos

--- src/data/pedido/PedidoDataSource.java
+++ src/data/pedido/PedidoDataSource.java
@@ -2,29 +2,101 @@ package data.pedido;
 
 import data.item.ItemEntity;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+import javax.management.RuntimeErrorException;
+import DB.DataBaseUtils;
+
+//import java.util.*;
+import java.sql.*;
+import java.time.LocalDate;
 
 
 // CRUD/Integracao do banco
 public class PedidoDataSource {
 
     public void createPedido(PedidoEntity pedidoEntity) {
+    String query = "INSERT INTO PEDIDO(data_abertura, data_fechamento, id_funcionario) VALUES (?, ?, ?)";
 
+    try {
+        DataBaseUtils.executeUpdate(query, pedidoEntity.dataAbertura(), pedidoEntity.dataFechamento(), pedidoEntity.idFuncionario());
+        System.out.println("Pedido criado com sucesso!");
+
+    } catch (SQLException e) {
+        throw new RuntimeException("Erro ao criar pedido: " + e.getMessage());
     }
+}
 
     public PedidoEntity getPedidoById(int id){
+        String query = "SELECT * FROM PEDIDO p where p.id = ?";
+
+        try{
+            ResultSet result = DataBaseUtils.executeQuery(query, id);
+            result.next();
+                Date dataAbertura = result.getDate(2);
+                LocalDate localDate = dataAbertura.toLocalDate();
+                Date dataFechamento = result.getDate(3);
+                LocalDate localDate2 =  dataFechamento.toLocalDate();
+                int idFuncionario = result.getInt(4);
+                int status = result.getInt(5);
+
+            PedidoEntity pedido = new PedidoEntity(localDate, localDate2, idFuncionario, status);
+            return pedido;
+            
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 
     public List<PedidoEntity> getPedidos(){
+        String query = "SELECT * FROM PEDIDO";
+        List<PedidoEntity> pedidosBanco = new ArrayList<>();
 
+        try{
+            ResultSet result = DataBaseUtils.executeQuery(query);
+            while(result.next()){
+                Date dataAbertura = result.getDate(2);
+                LocalDate localDate = dataAbertura.toLocalDate();
+                Date dataFechamento = result.getDate(3);
+                LocalDate localDate2 =  dataFechamento.toLocalDate();
+                int idFuncionario = result.getInt(4);
+                int status = result.getInt(5);
+                pedidosBanco.add(new PedidoEntity(localDate, localDate2, status, idFuncionario));
+            }  
+            
+            
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return pedidosBanco;
     }
 
-    public void updatePedido(int id){
+    public void updatePedido(int id, Status novoStatus){
+        String updateQuery = "UPDATE PEDIDO p SET STATUS = ? WHERE p.id_pedido = ? ";
 
+        try{
+            DataBaseUtils.executeUpdate(updateQuery, novoStatus, id);
+            
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+
+        }
     }
 
     public void deletePedido(int id){
+        String deleteQuery = "DELETE FROM PEDIDO p WHERE P.id = ?";
+        try{
+        DataBaseUtils.executeUpdate(deleteQuery, id);
+        System.out.println("Pedido excluído com sucesso!");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
